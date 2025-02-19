c74ec3b663903819b5ac677a449778f992830b7f
megustaelcampo <pablobejarano2005@gmail.com>
2025-02-19 09:16:46 +0100

    Add merchandising v1

--- JS/gif-move.js
+++
@@ -1,7 +0,0 @@
-function playGif() {
-    const img = document.getElementById('maidGif');
-    img.src = './img/header.gif';
-    setTimeout(() => {
-        img.src = './img/header_still.png';
-    }, 2800); // Adjust the timeout to match the duration of your GIF
-}
