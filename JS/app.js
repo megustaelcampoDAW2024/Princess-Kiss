3f470a27a482d6b91e9fa4d4721009d74ad378ff
megustaelcampo <pablobejarano2005@gmail.com>
2025-03-04 17:46:25 +0100

    Completado con informacion del resto de apartados

--- JS/app.js
+++ JS/app.js
@@ -2,7 +2,7 @@ const app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue!',
-        currentSection: 'estudio_mercado'
+        currentSection: 'tramites'
     },
     methods: {
         greet() {
